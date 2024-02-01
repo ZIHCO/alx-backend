ed6aa09987252bfd31fb9fba0c6deebe0391885f
zihco <chizobajames21@gmail.com>
2024-02-01 15:41:05 +0100

    lru policy

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -6,7 +6,7 @@ from base_caching import BaseCaching
 
 class LRUCache(BaseCaching):
     """subclass of BaseCaching"""
-    __MRC = ""
+    __MRC = []
 
     def __init__(self):
         """instantiate an object"""
@@ -17,33 +17,26 @@ class LRUCache(BaseCaching):
         if key is not None and item is not None:
             if self.is_compulsory_miss():
                 self.cache_data[key] = item
-                type(self).__MRC += key
+                type(self).__MRC.append(key)
             else:
                 if self.hit_validator(key):
                     self.cache_data[key] = item
-                    index = type(self).__MRC.find(key)
-                    if index == 0:
-                        lru_key = type(self).__MRC[0]
-                        type(self).__MRC = type(self).__MRC[1:]
-                        type(self).__MRC += lru_key
-                    else:
-                        lru_key = type(self).__MRC[index]
-                        type(self).__MRC = (type(self).__MRC[:index]
-                                            + type(self).__MRC[(index + 1):])
-                        type(self).__MRC += lru_key
-
+                    index = type(self).__MRC.index(key)
+                    lru_key = type(self).__MRC[index]
+                    del type(self).__MRC[index]
+                    type(self).__MRC.append(lru_key)
                 else:
                     print(f"DISCARD: {type(self).__MRC[0]}")
                     del self.cache_data[type(self).__MRC[0]]
                     self.cache_data[key] = item
-                    type(self).__MRC = type(self).__MRC[1:]
-                    type(self).__MRC += key
+                    del type(self).__MRC[0]
+                    type(self).__MRC.append(key)
 
     def get(self, key):
         """returns self.cache_data[key]"""
         if key is not None:
             try:
-                if type(self).__MRC.find(key) > 0:
+                """if type(self).__MRC.find(key) > 0:
                     index = type(self).__MRC.find(key)
                     if index == 0:
                         lru_key = type(self).__MRC[0]
@@ -53,7 +46,7 @@ class LRUCache(BaseCaching):
                         lru_key = type(self).__MRC[index]
                         type(self).__MRC = (type(self).__MRC[0:index]
                                             + type(self).__MRC[(index + 1):])
-                        type(self).__MRC += lru_key
+                        type(self).__MRC += lru_key"""
                 return self.cache_data[key]
             except KeyError:
                 return None
