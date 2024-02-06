4603448e01f991305797872ab4fc4192fcb04e13
zihco <chizobajames21@gmail.com>
2024-02-06 22:52:32 +0100

    render a html page with flask

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
-"""A Basic Flask app.
-"""
+"""app setup"""
+from flask import Flask
+from flask import render_template
 from flask_babel import Babel
-from flask import Flask, render_template
 
 
 class Config:
-    """Represents a Flask Babel configuration.
-    """
+    """language config in app"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
@@ -15,16 +14,15 @@ class Config:
 
 app = Flask(__name__)
 app.config.from_object(Config)
-app.url_map.strict_slashes = False
+
 babel = Babel(app)
 
 
-@app.route('/')
-def get_index() -> str:
-    """The home/index page.
-    """
-    return render_template('1-index.html')
+@app.route('/', strict_slashes=False)
+def hello():
+    """index page"""
+    return render_template("1-index.html")
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(port=5000, host='0.0.0.0')
