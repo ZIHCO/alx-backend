b8953e05910c93eadcb060a5b9f11321d1966270
zihco <chizobajames21@gmail.com>
2024-02-06 16:44:38 +0100

    render a html page with flask

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""app setup"""
+from flask import Flask
+from flask import render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+@app.route('/')
+def hello():
+    """index page"""
+    return render_template("1-index.html")
+
+
+class Config:
+    """language config in app"""
+    LANGUAGE = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
