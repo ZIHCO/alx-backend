40a4e362cda84e4ee8d4e907a3c41511ff68820d
zihco <chizobajames21@gmail.com>
2024-02-06 23:24:15 +0100

    done

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -17,16 +17,16 @@ app.config.from_object(Config)
 babel = Babel(app)
 
 
-@app.route('/', strict_slashes=False)
-def hello() -> str:
-    """index page"""
-    return render_template("2-index.html")
-
-
 @babel.localeselector
 def get_locale() -> str:
     """determines best match for language"""
     return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+@app.route('/', strict_slashes=False)
+def hello() -> str:
+    """index page"""
+    return render_template("2-index.html")
 
 
 if __name__ == "__main__":
