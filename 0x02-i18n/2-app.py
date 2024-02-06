956dc425c736d1ae1b4fcdfb4b733a852a323190
zihco <chizobajames21@gmail.com>
2024-02-06 23:33:11 +0100

    done

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
-"""contains get_locale function"""
+"""A Basic Flask app.
+"""
 from flask_babel import Babel
 from flask import Flask, render_template, request
 
 
 class Config:
-    """language config in app"""
+    """Represents a Flask Babel configuration.
+    """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
@@ -13,21 +15,23 @@ class Config:
 
 app = Flask(__name__)
 app.config.from_object(Config)
-
+app.url_map.strict_slashes = False
 babel = Babel(app)
 
 
 @babel.localeselector
 def get_locale() -> str:
-    """determines best match for language"""
+    """Retrieves the locale for a web page.
+    """
     return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
-@app.route('/', strict_slashes=False)
-def hello() -> str:
-    """index page"""
-    return render_template("2-index.html")
+@app.route('/')
+def get_index() -> str:
+    """The home/index page.
+    """
+    return render_template('2-index.html')
 
 
-if __name__ == "__main__":
-    app.run(port=5000, host='0.0.0.0')
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
