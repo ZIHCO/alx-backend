5259c597ae07afe24a96a2501661eab6250f8088
zihco <chizobajames21@gmail.com>
2024-06-13 20:38:20 +0000

    done

--- 0x03-queuing_system_in_js/6-job_creator.js
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -8,7 +8,7 @@ const jobData = {
 }
 
 const job = queue.create('push_notification_code', jobData)
-  .on('enqueue', () => console.log(`Notification job created: ${job.data.message}`))
+  .on('enqueue', () => console.log(`Notification job created: ${job.id}`))
   .on('complete', () => console.log('Notification job completed'))
   .on('failed attempt', () => console.log('Notification job failed'))
   .save();
