77a5fab67d6717971294821404b08eb8ece0fe1e
zihco <chizobajames21@gmail.com>
2024-06-13 04:23:28 +0000

    done

--- 0x03-queuing_system_in_js/1-redis_op.js
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -7,11 +7,11 @@ client.on('connect', () => console.log('Redis client connected to the server'));
 client.on('error', (error) => console.log(`Redis client not connected to the server: ${error}`));
 
 function setNewSchool(schoolName, value) {
-  client.set(schoolName, value, print);
+  client.SET(schoolName, value, print);
 }
 
 function displaySchoolValue(schoolName) {
-  client.get(schoolName, (err, resp) => {
+  client.GET(schoolName, (err, resp) => {
     if (!err) {
       console.log(resp);
     }
--- 0x03-queuing_system_in_js/2-redis_op_async.js
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -8,12 +8,12 @@ client.on('connect', () => console.log('Redis client connected to the server'));
 client.on('error', (error) => console.log(`Redis client not connected to the server: ${error}`));
 
 function setNewSchool(schoolName, value) {
-  client.set(schoolName, value, print);
+  client.SET(schoolName, value, print);
 }
 
 async function displaySchoolValue(schoolName) {
-  client.get = promisify(client.get);
-  await client.get(schoolName)
+  client.GET = promisify(client.GET);
+  await client.GET(schoolName)
     .then((resp) => console.log(resp))
     .catch((error) => console.log(error));
 }
