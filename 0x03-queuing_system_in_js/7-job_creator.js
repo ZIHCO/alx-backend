71028da5469164ca311b92d9fd2a8f581f2a087f
zihco <chizobajames21@gmail.com>
2024-06-14 20:27:58 +0000

    done

--- 0x03-queuing_system_in_js/7-job_creator.js
+++ 0x03-queuing_system_in_js/7-job_creator.js
@@ -3,48 +3,48 @@ import { createQueue } from 'kue';
 const jobs = [
   {
     phoneNumber: '4153518780',
-    message: 'This is the code 1234 to verify your account'
+    message: 'This is the code 1234 to verify your account',
   },
   {
     phoneNumber: '4153518781',
-    message: 'This is the code 4562 to verify your account'
+    message: 'This is the code 4562 to verify your account',
   },
   {
     phoneNumber: '4153518743',
-    message: 'This is the code 4321 to verify your account'
+    message: 'This is the code 4321 to verify your account',
   },
   {
     phoneNumber: '4153538781',
-    message: 'This is the code 4562 to verify your account'
+    message: 'This is the code 4562 to verify your account',
   },
   {
     phoneNumber: '4153118782',
-    message: 'This is the code 4321 to verify your account'
+    message: 'This is the code 4321 to verify your account',
   },
   {
     phoneNumber: '4153718781',
-    message: 'This is the code 4562 to verify your account'
+    message: 'This is the code 4562 to verify your account',
   },
   {
     phoneNumber: '4159518782',
-    message: 'This is the code 4321 to verify your account'
+    message: 'This is the code 4321 to verify your account',
   },
   {
     phoneNumber: '4158718781',
-    message: 'This is the code 4562 to verify your account'
+    message: 'This is the code 4562 to verify your account',
   },
   {
     phoneNumber: '4153818782',
-    message: 'This is the code 4321 to verify your account'
+    message: 'This is the code 4321 to verify your account',
   },
   {
     phoneNumber: '4154318781',
-    message: 'This is the code 4562 to verify your account'
+    message: 'This is the code 4562 to verify your account',
   },
   {
     phoneNumber: '4151218782',
-    message: 'This is the code 4321 to verify your account'
-  }
+    message: 'This is the code 4321 to verify your account',
+  },
 ];
 
 const queue = createQueue();
@@ -54,6 +54,6 @@ jobs.forEach((entry) => {
     .on('enqueue', () => console.log(`Notification job created: ${job.id}`))
     .on('complete', () => console.log(`Notification job ${job.id} completed`))
     .on('failed', (error) => console.log(`Notification job ${job.id} failed: ${error}`))
-    .on('progress', (prog, data) => console.log(`Notification job ${job.id} ${prog}% complete`))
+    .on('progress', (prog) => console.log(`Notification job ${job.id} ${prog}% complete`))
     .save();
 });
